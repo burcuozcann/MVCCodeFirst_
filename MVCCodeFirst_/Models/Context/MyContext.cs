5fcfd12f6ac23925f12a38d53d33248fc0ca7c54
burcu ozcan <burcuozcann91@gmail.com>
2023-01-19 00:01:33 +0000

    Add project files.

---
+++ MVCCodeFirst_/Models/Context/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCCodeFirst_.Models.Context
+{
+    public class MyContext:DbContext
+    {
+        public MyContext():base("MyConnection")
+        {
+
+        }
+    }
+}
---
+++ MVCCodeFirst_/Models/Enums/UserRole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCCodeFirst_.Models.Enums
+{
+    public enum UserRole
+    {
+        Admin = 1 , Member = 2 , Visitor = 3
+    }
+}
